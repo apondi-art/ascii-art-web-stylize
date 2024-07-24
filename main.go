8fd1ba2f8bf48ef52620a801e229f38480323fdc
hilaokello <hilaryokello091@gmail.com>
2024-07-24 16:54:46 +0300

    fix: incorporated about page

--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"ascii-art-web/handlers"
 )
 
 func main() {
+	if len(os.Args) != 1 {
+		log.Fatal("Usage: 'go run .'")
+	}
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", handlers.GetAsciiForm)
+	http.HandleFunc("/about", handlers.GetAbout)
 	http.HandleFunc("/ascii-art", handlers.PostAsciiArt)
 	fmt.Println("SUCCESS!! listen to server at http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
